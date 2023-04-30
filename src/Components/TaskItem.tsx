01a96ceb930dc0e027b1327ae2de0c4401201ff1
online-marathon <61456363+online-marathon@users.noreply.github.com>
2023-04-30 16:39:30 +0300

    Initial commit

---
+++ src/Components/TaskItem.tsx
@@ -0,0 +1,43 @@
+export type Task = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+type TaskProps = {
+  task: Task;
+  onRemove: (id: number) => void;
+  onToggle: (id: number) => void;
+};
+
+const TaskItem = ({ task, onRemove, onToggle }: TaskProps): JSX.Element => {
+  const handleToggle = () => {
+    onToggle(task.id);
+  };
+
+  const handleRemove = () => {
+    onRemove(task.id);
+  };
+
+  return (
+    <li className="task-item">
+      <input
+        className="task-item-checkbox"
+        type="checkbox"
+        checked={task.completed}
+        onChange={handleToggle}
+      />
+      <span
+        className="task-item-text"
+        style={{ textDecoration: task.completed ? "line-through" : "none" }}
+      >
+        {task.text}
+      </span>
+      <button className="task-item-remove-button" onClick={handleRemove}>
+        Remove
+      </button>
+    </li>
+  );
+};
+
+export default TaskItem;
---
+++ src/Components/TaskList.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import TaskItem, { Task } from "./TaskItem";
+
+type TaskListProps = { tasks?: Task[] };
+
+const TaskList = (props: TaskListProps): JSX.Element => {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    if (props.tasks) {
+      return props.tasks;
+    }
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      return JSON.parse(storedTasks);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleNewTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() === "") return;
+    const newId = tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1;
+    const newTaskItem = { id: newId, text: newTask, completed: false };
+    setTasks(existingTasks => [...existingTasks, newTaskItem]);
+    setNewTask("");
+  };
+
+  const handleRemoveTask = (taskId: number) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const handleTaskStatusChange = (taskId: number) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  return (
+    <div className="task-list">
+      <h2 className="task-list-title">Task List</h2>
+      <div className="add-task-form">
+        <input
+          className="add-task-input"
+          type="text"
+          value={newTask}
+          onChange={handleNewTaskChange}
+        />
+        <button className="add-task-button" onClick={handleAddTask}>
+          Add
+        </button>
+      </div>
+      {tasks.length > 0 ? (
+        <ul>
+          {tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              onRemove={handleRemoveTask}
+              onToggle={handleTaskStatusChange}
+            />
+          ))}
+        </ul>
+      ) : (
+        <div className="task-list-empty">No tasks to display</div>
+      )}
+    </div>
+  );
+};
+
+export default TaskList;
